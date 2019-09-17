9b65d3011c08ecf8a2f5fd3df6ce2e8985842d2d
Louisejesuis <louise.roy@hotmail.fr>
2019-09-17 09:37:23 +0200

    Add web app

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 
-const { Item } = require('./data-layer/models');
+const { Item } = require("./data-layer/models");
 
 const app = express();
 
-app.get('/api/items', async (req, res) => {
-  res.send({ items: await Item.findAll() });
+app.get("/api/items", async (req, res) => {
+    res.send({ items: await Item.findAll() });
 });
 
 const PORT = process.env.PORT || 8007;
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}.`);
+    console.log(`Listening on port ${PORT}.`);
 });
---
+++ web-app/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = { items: [] };
+
+  async componentDidMount() {
+    const response = await fetch('/api/items');
+    console.log(response)
+    const { items } = await response.json();
+    this.setState({ items });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          {this.state.items.map(item => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
